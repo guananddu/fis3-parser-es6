990b2f9ae3b1d544aad534790481eb63a89a270f
Pooky <guananddu@qq.com>
2016-01-27 18:52:07 +0800

    bug fix

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ module.exports = function ( content, file, settings ) {
     var result = transform( content, _.extend( {
         presets: [
             require.resolve( 'babel-preset-es2015' ),
-            require.resolve( 'babel-preset-react' ),
-            require.resolve( 'babel-preset-stage-0' )
+            require.resolve( 'babel-preset-stage-0' ),
+            require.resolve( 'babel-preset-react' )
         ]
     }, settings ) );
     return result.code;
