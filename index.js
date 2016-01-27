cd34f2ebba078ac50c09c81191f6adc151f2aaed
Pooky <guananddu@qq.com>
2016-01-27 18:49:25 +0800

    bug fix

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 // fis3 for es6
 
-require( 'babel-preset-es2015' );
-require( 'babel-preset-react' );
-
 const _ = require( 'underscore' );
 const babel = require( 'babel-core' );
 const transform = babel.transform;
 
 module.exports = function ( content, file, settings ) {
     var result = transform( content, _.extend( {
-        presets: [ 'es2015', 'react' ]
+        presets: [
+            require.resolve( 'babel-preset-es2015' ),
+            require.resolve( 'babel-preset-react' ),
+            require.resolve( 'babel-preset-stage-0' )
+        ]
     }, settings ) );
     return result.code;
 };
